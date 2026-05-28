88f48bda232265427b4b82fcc4d066c78526c87b
Joniprog <jonathanzwierlein@gmail.com>
2026-05-28 22:31:30 +0200

    print sections

--- dns/dns_server.py
+++ dns/dns_server.py
@@ -3,6 +3,8 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("", 53))
 while True:
     msg = dq.receive_udp(sock)
-    print(msg)
+    for a in msg.sections:
+        for b in a:
+            print(b.to_styled_text())
     resp = dq.udp(msg[0], "8.8.8.8")
     dq.send_udp(sock, resp, msg[2])
